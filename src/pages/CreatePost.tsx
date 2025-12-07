3b5e12d5b0be66655edcd3e74efba620d06c3388
jangracner <jangracner@gmail.com>
2025-12-07 10:16:27 +0100

    Frontend ready for KUbernetes deployment on main branch, 7.12.

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import MainHeader from '../components/mainHeader';
-import '../styles/CreatePost.css'
+import '../styles/createPost.css'
 import '../styles/feed.css'
 import { useNavigate } from 'react-router-dom'
 import { Container } from 'react-bootstrap';
